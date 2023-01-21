a00b2207c3ac7993da763dc26ab36bcdb784d2b1
caykay <kahemacassian@gmail.com>
2023-01-21 17:10:30 +1030

    UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Fragment, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+function DiceElements(props) {
+  return (
+    <div className="dice-group">
+      {[...Array(10).keys()].map((i) => (
+        <div className={`die ${i + 1}`} key={i + 1}>
+          {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
 
-  return <div className="App"></div>;
+function App() {
+  return (
+    <main className="App">
+      <section className="description">
+        <h2 className="description--title">Tenzies</h2>
+        <p className="description--content">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </section>
+      <DiceElements />
+      <button className="btn">Roll</button>
+    </main>
+  );
 }
 
 export default App;
