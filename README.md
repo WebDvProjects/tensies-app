a889a1722e8e40de69ceb819db87077803d52f71
caykay <kahemacassian@gmail.com>
2023-01-21 19:16:26 +1030

    End Game

---
+++ README.md
@@ -0,0 +1,25 @@
+# TENZIES
+
+A simple react app that simulates a game of tenzies.
+
+This was a simple project to explore and understand how hooks in react perform and some of its cool features.
+
+[Live](https://webdvprojects.github.io/tenzies-app/)
+
+## How to play
+
+- The goal of the game is to roll all 10 of your dice and have them all show the same number.
+
+- Continue to roll and set aside matching dice until all 10 dice show the same number.
+
+- Dice set aside will have a different color from the rest.
+
+- You can also un-select a die that is already being held
+
+## Preview
+
+<img src="images-public/tenzies_1.JPG" alt="Screenshot" width="50%"/>
+
+<img src="images-public/tenzies_2.JPG" alt="Screenshot" width="50%"/>
+
+<img src="images-public/tenzies_3.JPG" alt="Screenshot" width="50%"/>
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Fragment, useState } from "react";
 import "./App.css";
+import { nanoid } from "nanoid";
+import { useEffect } from "react";
+import Confetti from "react-confetti";
 
 function DiceElements(props) {
   return (
     <div className="dice-group">
-      {[...Array(10).keys()].map((i) => (
-        <div className={`die ${i + 1}`} key={i + 1}>
-          {i + 1}
+      {props.diceNums.map((die) => (
+        <div
+          // if die is isHeld then add selected class in
+          className={`die`}
+          data-is-held={die.isHeld}
+          key={die.id}
+          onClick={() => props.handleClick(die.id)}>
+          {die.value}
         </div>
       ))}
     </div>
@@ -14,17 +22,85 @@ function DiceElements(props) {
 }
 
 function App() {
+  const [dice, setDice] = useState(() => getRandomDiceNums());
+
+  function getRandomDiceNums() {
+    // generate 10 random numbers btn 1-6 inclusive
+    return [...Array(10)].map((v, k) => ({
+      id: k + 1,
+      value: Math.floor(Math.random() * 6 + 1),
+      isHeld: false,
+    }));
+  }
+
+  function handleSelect(dieId) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        // look for the die with a matching id and select it
+        die.id === dieId ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  // WIN STATE
+  const [tenzies, setTenzies] = useState(false);
+
+  // check win
+  useEffect(() => {
+    // isWin will loop through the dice and store the current
+    // die.value and another value which returns true if all
+    // values are the same and are held
+    const win = dice.reduce(
+      (isWin, die) => [
+        die.value,
+        isWin[0] === die.value && isWin[1] && die.isHeld,
+      ],
+      [dice[0].value, dice[0].isHeld]
+    );
+
+    if (win[1]) {
+      console.log("YOU WON");
+      setTenzies(win[1]);
+    }
+  }, [dice]);
+
+  function rollDice() {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        // for every die check if die is isHeld, if yes then dont change
+        die.isHeld ? die : { ...die, value: Math.floor(Math.random() * 6) }
+      )
+    );
+  }
+
+  function newGame() {
+    setTenzies(false);
+    setDice(() => getRandomDiceNums());
+  }
+
   return (
     <main className="App">
+      {tenzies && <Confetti />}
       <section className="description">
         <h2 className="description--title">Tenzies</h2>
         <p className="description--content">
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
+        {tenzies && (
+          <h3
+            style={{
+              margin: "0",
+              marginTop: "20px",
+            }}>
+            YOU WON! YAYYYYy
+          </h3>
+        )}
       </section>
-      <DiceElements />
-      <button className="btn">Roll</button>
+      <DiceElements diceNums={dice} handleClick={handleSelect} />
+      <button className="btn" onClick={tenzies ? newGame : rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   );
 }
